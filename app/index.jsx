ff617bdef5c5f61700110a24443399f1e2681b12
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-06-28 16:40:54 -0400

    buttons displayed on main page and title

--- app/index.jsx
+++ app/index.jsx
@@ -6,17 +6,17 @@ import {Link} from 'expo-router'
 
 const index = () => {
   return (
-    <View>
+    <View style={styles.container}>
 
+        <Text style={styles.title}>RareToys</Text>
 
 
     {/*Links for each page that is created, basically a button to go to that page will be displayed on this screen. */}
-      <Link href="/login">Login</Link>
-      <Link href="/toys">Toys</Link>
-      <Link href="/stores">Stores</Link>
-      <Link href="/camera">Camera</Link>
-      <Link href="/feedback">Feedback</Link>
-      
+      <Link href="/login"   style={styles.mainScreenButton}>Login</Link>
+      <Link href="/toys"    style={styles.mainScreenButton}>Toys</Link>
+      <Link href="/stores"  style={styles.mainScreenButton}>Stores</Link>
+      <Link href="/camera"  style={styles.mainScreenButton}>Camera</Link>
+      <Link href="/feedback"style={styles.mainScreenButton}>Feedback</Link>
 
     </View>
   )
@@ -25,15 +25,26 @@ const index = () => {
 export default index
 
 const styles = StyleSheet.create({
-
+    
     container:{
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:'#ffff',
     },
     mainScreenButton:{
         backgroundColor: '#3A5F0B',
         paddingVertical: 10,
         paddingHorizontal: 20,
         borderRadius: 5,
-        marginTop: 20
+        marginTop: 20,
     },
+    title:{
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'Black',
+        textShadowColor: '#000',
+        textShadowOffset: { width: 1, height: 1 },
+        textShadowRadius: 2,
+    }
 })
