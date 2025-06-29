65604b976ad2d4cc5fa1cc0b1784631a02e259f7
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-06-28 20:37:32 -0400

    got rid of link along with other components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,5 @@
 //this is the main point of the application where you add all components to this page
-import { StyleSheet, Text, View, Linking, Pressable, Image} from 'react-native'
-
-//link for to attach to different screens
-import {Link} from 'expo-router'
+import { StyleSheet, Text, View} from 'react-native'
 
 const layout = () => {
   return (
