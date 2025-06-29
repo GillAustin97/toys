734da891e9724ae30963cda9c1bc1b3c925c203b
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-06-28 22:47:08 -0400

    adding camera since we cant get footer to work at the moment and installed expo camera

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,15 +6,15 @@ const layout = () => {
 
   return (
     <Stack screenOptions={{
-          
+
     }}>
 
       <Stack.Screen name="index" options={{title: 'Home'}} />
-      <Stack.Screen name="login" options={{title: 'Home'}} />
-      <Stack.Screen name="stores" options={{title: 'Home'}} />
-      <Stack.Screen name="toys" options={{title: 'Home'}} />
-      <Stack.Screen name="feedback" options={{title: 'Home'}} />
-      <Stack.Screen name="camera" options={{title: 'Home'}} />
+      <Stack.Screen name="login" options={{title: 'Login'}} />
+      <Stack.Screen name="stores" options={{title: 'Stores'}} />
+      <Stack.Screen name="toys" options={{title: 'Toys'}} />
+      <Stack.Screen name="feedback" options={{title: 'Feedback'}} />
+      <Stack.Screen name="camera" options={{title: 'Camera'}} />
     
     </Stack>
   )
