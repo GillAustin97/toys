db6550990986859a904b44b6b70a69646adc2f53
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-06-28 20:53:16 -0400

    adding a footer to the application, not finished

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,34 @@
 //this is the main point of the application where you add all components to this page
 import { StyleSheet, Text, View} from 'react-native'
 
+import { Stack } from 'expo-router'
+
 const layout = () => {
   return (
-    <View>
-      <Text>layout</Text>
-    </View>
+
+    <Stack screenOptions={{
+        headerStyle:{
+            backgroundColor:'#000000'
+        },
+        headerTintColor:{
+            
+        },
+
+
+    }}>
+        <Stack.Screen name="index" options={{ title: 'Home'}} />
+        <Stack.Screen name="login" options={{ title: 'Login'}} />
+        <Stack.Screen name="stores" options={{ title: 'Stores'}} />
+        <Stack.Screen name="toys" options={{ title: 'Toys'}} />
+        <Stack.Screen name="feedback" options={{ title: 'Feedback'}} />
+        <Stack.Screen name="camera" options={{ title: 'Camera'}} />
+
+
+
+
+
+    </Stack>
+
   )
 }
 
