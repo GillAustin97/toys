a7d673c7c34f0c42ecfe7fa602053b866260bc54
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-06-28 21:09:31 -0400

    restarting footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,29 +6,6 @@ import { Stack } from 'expo-router'
 const layout = () => {
   return (
 
-    <Stack screenOptions={{
-        headerStyle:{
-            backgroundColor:'#000000'
-        },
-        headerTintColor:{
-            
-        },
-
-
-    }}>
-        <Stack.Screen name="index" options={{ title: 'Home'}} />
-        <Stack.Screen name="login" options={{ title: 'Login'}} />
-        <Stack.Screen name="stores" options={{ title: 'Stores'}} />
-        <Stack.Screen name="toys" options={{ title: 'Toys'}} />
-        <Stack.Screen name="feedback" options={{ title: 'Feedback'}} />
-        <Stack.Screen name="camera" options={{ title: 'Camera'}} />
-
-
-
-
-
-    </Stack>
-
   )
 }
 
---
+++ themes/colors.js
@@ -0,0 +1,27 @@
+//This is for color themes that i want to keep the same
+
+
+export const Colors = {
+  primary: "#6849a7",
+  warning: "#cc475a",
+
+  dark: {
+    text: "#d4d4d4",
+    title: "#fff",
+    background: "#252231",
+    navBackground: "#3A5F0B",
+    iconColor: "#9591a5",
+    iconColorFocused: "#fff",
+    uiBackground: "#2f2b3d",
+  },
+
+  light: {
+    text: "#625f72",
+    title: "#201e2b",
+    background: "#e0dfE8",
+    navBackground: "#6F8F3C",
+    iconColor: "#686477",
+    iconColorFocused: "#201e2b",
+    uiBackground: "#d6d5e1",
+  },
+};
