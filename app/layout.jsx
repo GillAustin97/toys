ca5b75f1d9dee0908d3d5551086b68030ab31129
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-06-28 20:36:53 -0400

    layout file made.

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+//this is the main point of the application where you add all components to this page
+import { StyleSheet, Text, View, Linking, Pressable, Image} from 'react-native'
+
+//link for to attach to different screens
+import {Link} from 'expo-router'
+
+const layout = () => {
+  return (
+    <View>
+      <Text>layout</Text>
+    </View>
+  )
+}
+
+export default layout
+
+const styles = StyleSheet.create({})
