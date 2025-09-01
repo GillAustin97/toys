8f17d79051a3ad88736438ec4951ef0a3c3477eb
GillAustin97 <169080532+GillAustin97@users.noreply.github.com>
2025-08-31 21:57:15 -0400

    Added more stores for user experience

--- app/stores.jsx
+++ app/stores.jsx
@@ -15,7 +15,17 @@ import mercariLogo from '../assets/storeLogos/logo_mercari.png';
 import legoLogo from '../assets/storeLogos/logo_lego.png';
 import ebayLogo from '../assets/storeLogos/logo_ebay.png';
 import fbMarketplaceLogo from '../assets/storeLogos/logo_facebookMarketplace.png';
-
+import disneylogo from '../assets/storeLogos/logo_disney.png';
+import gamestoplogo from '../assets/storeLogos/logo_gamestop.png';
+import universallogo from '../assets/storeLogos/logo_universal.png';
+import hotTopiclogo from '../assets/storeLogos/logo_hotTopic.png';
+import boxLunchlogo from '../assets/storeLogos/logo_boxLunch.png';
+import toysRusLogo from '../assets/storeLogos/logo_ToysRus.png';
+import amazonLogo from '../assets/storeLogos/logo_amazon.png';
+import barnesLogos from '../assets/storeLogos/logo_BnN.png';
+import hasbroLogo from '../assets/storeLogos/logo_hasbro.png';
+import costcologo from '../assets/storeLogos/logo_costco.png';  
+import samsLogo from '../assets/storeLogos/logo_sams.png';
 
 const stores = () => {
 
@@ -40,6 +50,39 @@ const stores = () => {
   const fbMarketplaceSite = () => {
     Linking.openURL('https://www.facebook.com/marketplace')
   };
+  const disneySite = () => {
+    Linking.openURL('https://www.disneystore.com/')
+  };
+  const gamestopSite = () => {
+    Linking.openURL('https://www.gamestop.com/')
+  };
+  const universalSite = () => {
+    Linking.openURL('https://shop.universalorlando.com/shop/')
+  };
+  const hotTopicSite = () => {
+    Linking.openURL('https://www.hottopic.com/')
+  };
+  const boxLunchSite = () => {
+    Linking.openURL('https://www.boxlunch.com/')
+  }
+  const toysRusSite = () => {
+    Linking.openURL('https://www.toysrus.com/')
+  }
+  const amazonSite = () => {
+    Linking.openURL('https://www.amazon.com/')
+  }
+  const barnesSite = () => {
+    Linking.openURL('https://www.barnesandnoble.com/')
+  }
+  const hasbroSite = () => {
+    Linking.openURL('https://shop.hasbro.com/en-us')
+  }
+  const costcoSite = () => {
+    Linking.openURL('https://www.costco.com/')
+  }
+  const samsSite = () => {
+    Linking.openURL('https://www.samsclub.com/')
+  }
 
   return (
 
@@ -72,6 +115,50 @@ const stores = () => {
 
       <Pressable style={styles.button} onPress={fbMarketplaceSite}>
         <Image source={fbMarketplaceLogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={disneySite}>
+        <Image source={disneylogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={gamestopSite}>
+        <Image source={gamestoplogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={universalSite}>
+        <Image source={universallogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={hotTopicSite}>
+        <Image source={hotTopiclogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={boxLunchSite}>
+        <Image source={boxLunchlogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={toysRusSite}>
+        <Image source={toysRusLogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={amazonSite}>
+        <Image source={amazonLogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={barnesSite}>
+        <Image source={barnesLogos} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={hasbroSite}>
+        <Image source={hasbroLogo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={costcoSite}>
+        <Image source={costcologo} style={styles.logo} />
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={samsSite}>
+        <Image source={samsLogo} style={styles.logo} />
       </Pressable>
 
     </View>
